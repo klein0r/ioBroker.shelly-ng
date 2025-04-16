d69b2767d967793bba941e9e3f6927ada51e3b5a
Matthias Kleine <github.com9@emailu.de>
2025-04-16 15:21:43 +0200

    eslint config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,6 +3,4 @@ import prettierConfig from '@iobroker/eslint-config/prettier.config.mjs';
 
 export default {
     ...prettierConfig,
-    // uncomment next line if you prefer double quotes
-    // singleQuote: false,
 }
