510db9e589a1ef8871e62bf1ff0d085b46db44de
Matthias Kleine <github.com9@emailu.de>
2025-05-29 10:58:40 +0200

    Support Gen4

--- src/lib/device/base.ts
+++ src/lib/device/base.ts
@@ -76,6 +76,7 @@ export abstract class BaseDevice {
                     1: 'Generation 1',
                     2: 'Generation 2',
                     3: 'Generation 3',
+                    4: 'Generation 4',
                 },
             },
             native: {},
